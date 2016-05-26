db132c9cb0225113bb3fb302e17df4ae59013a7d
leegheid <skv.95@mail.ru>
2016-05-26 18:44:33 +0300

    deleted js files

--- public_html/js/engine/card_collection.js
+++ public_html/js/engine/card_collection.js
@@ -1,6 +1,10 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+        throw new TypeError("Cannot call a class as a function");
+    }
+}
 
 define(['jquery', 'underscore', 'backbone', 'settings', 'pixi', './CardModel'], function ($, _, Backbone, Settings, pixi, Card) {
     var CardCollection = function CardCollection(loaderRes, size) {
